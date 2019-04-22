2aee28641f116047fcc3820466ec48b622b9778c
GOVIND KUMAR <39936384+govind2006@users.noreply.github.com>
2019-04-22 15:14:14 +0530

    Update trapezoidal rule.c

--- trapezoidal rule.c
+++ trapezoidal rule.c
@@ -1,25 +1,25 @@
 // question f(x)=1/(1+x*x)
 #include <stdio.h>
-double y[1000];
-
-double func(int i) {
-  double sum, x = i;
-  sum = 1 / (1 + x * x);
+double func(double x) {
+  double sum;
+  sum = 1 / (1 + x*x);
   return sum;
 }
 
 int main() {
-  int a, b, i;
-  double sum = 0, h;
+  int a, b;
+  double sum = 0, h,i;
   printf("enter the limits=");
   scanf("%d%d", &a, &b);
   printf("enter the value of differences=");
   scanf("%lf", &h);
-  for (i = a; i <= b; i = i + h)
-    y[i] = func(i);
-  for (i = a + 1; i < b; i = i + h)
-    sum = sum + 2 * y[i];
-  sum = sum + y[a] + y[b];
+  i=a+h;
+  while(i<b)
+  {
+  sum = sum + 2 * func(i);
+  i=i+h;
+  }
+   sum = sum + func(a) + func(b);
   sum = h / 2 * (sum);
   printf("soln=%lf", sum);
 }
