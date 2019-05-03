391dd9f13de0ed61ecd46bbd3135597fea0a8de0
GOVIND KUMAR <39936384+govind2006@users.noreply.github.com>
2019-05-03 15:40:27 +0530

    Update matrix chain multiplication.c

--- matrix chain multiplication.c
+++ matrix chain multiplication.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<limits.h>
 int p[1000], A[1000], m[1000][1000], s[1000][1000], n;
 void matchmul() {
   int i, l, j, k, q;
@@ -30,17 +31,38 @@ void print_optimal_parens(int i, int j) {
   }
 }
 
+int order(int i, int j)
+{
+    if(i == j)
+        return 0;
+    int k;
+    int min = INT_MAX;
+    int count;
+
+    for (k = i; k <j; k++)
+    {
+        count = order(i, k) +
+                order(k+1, j) +
+                p[i-1]*p[k]*p[j];
+
+        if (count < min)
+            min = count;
+    }
+    return min;
+}
+
 int main() {
   int i, j;
   printf("enter the size=");
   scanf("%d", &n);
-  printf("enter the value=");
+  printf("enter the value=\n");
   for (i = 0; i <= n; i++)
     scanf("%d", &p[i]);
   for (i = 1; i <=n; i++)
     A[i] = p[i-1] * p[i];
 
   matchmul();
+  printf("\n");
   for (i = 1; i <= n; i++) {
     for (j = i + 1; j <= n; j++)
       printf("%d ", s[i][j]);
@@ -52,5 +74,5 @@ int main() {
     printf("\n");
   }
   print_optimal_parens(1, n);
+  printf("\nMinimum multiplications=: %d \n",order(1, n));
 }
-
